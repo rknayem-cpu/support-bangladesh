e508b89feead5f6ebebfac4594984180d1c160d6
rknayem-cpu <nayemlx.mab@gmail.com>
2025-08-30 10:30:11 +0000

    upload upadate

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
-const app = require('../app');
+const serverless = require('serverless-http');
+const app = require('../app');  // আপনার মূল Express অ্যাপ
 
-module.exports = app;
+module.exports.handler = serverless(app);  // Express অ্যাপকে serverless ফাংশন হিসেবে এক্সপোর্ট করা
